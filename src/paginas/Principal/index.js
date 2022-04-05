eda0ccb1b1b48eccd847925b57da09e04fa42097
Bruno <pj_antoniobruno@bocombbm.com.br>
2022-04-05 08:21:11 -0300

    Atualizações aula03 - 05040821

--- src/paginas/Principal/index.js
+++ src/paginas/Principal/index.js
@@ -9,60 +9,73 @@ import {
   ScrollView,
 } from "react-native";
 import estilos from "./estilos";
-import api from "../../services/api";
+
+import { buscaUsuario } from "../../services/requisicoes/usuarios";
 
 export default function Principal({ navigation }) {
   const [nomeUsuario, setNomeUsuario] = useState("");
   const [usuario, setUsuario] = useState({});
 
-  function Busca() {
-    api
-      .get("/users")
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  async function busca() {
+    const resultado = await buscaUsuario(nomeUsuario);
+
+    setNomeUsuario("");
+    if (resultado) {
+      setUsuario(resultado);
+      // console.log(resultado);
+    } else {
+      Alert.alert("Usuario nao encontrado");
+      setUsuario({});
+    }
   }
 
   return (
     <ScrollView>
       <View style={estilos.container}>
-        <>
-          <View style={estilos.fundo} />
-          <View style={estilos.imagemArea}>
-            <Image
-              source={{
-                uri: "https://github.com/abruno36/EscolaWeb/blob/master/.images/Bruno.jpg",
-              }}
-              style={estilos.imagem}
-            />
-          </View>
-          <Text style={estilos.textoNome}>Antonio Bruno</Text>
-          <Text style={estilos.textoEmail}>abruno36@gmail.com</Text>
-          <View style={estilos.seguidoresArea}>
-            <View style={estilos.seguidores}>
-              <Text style={estilos.seguidoresNumero}>30</Text>
-              <Text style={estilos.seguidoresTexto}>Seguidores</Text>
+        {usuario?.login && (
+          <>
+            <View style={estilos.fundo} />
+            <View style={estilos.imagemArea}>
+              <Image
+                source={{ uri: usuario.avatar_url }}
+                style={estilos.imagem}
+              />
             </View>
-            <View style={estilos.seguidores}>
-              <Text style={estilos.seguidoresNumero}>40</Text>
-              <Text style={estilos.seguidoresTexto}>Seguindo</Text>
+            <Text style={estilos.textoNome}>{usuario.name}</Text>
+            <Text style={estilos.textoEmail}>{usuario.email}</Text>
+            <View style={estilos.seguidoresArea}>
+              <View style={estilos.seguidores}>
+                <Text style={estilos.seguidoresNumero}>
+                  {usuario.followers}
+                </Text>
+                <Text style={estilos.seguidoresTexto}>Seguidores</Text>
+              </View>
+              <View style={estilos.seguidores}>
+                <Text style={estilos.seguidoresNumero}>
+                  {usuario.following}
+                </Text>
+                <Text style={estilos.seguidoresTexto}>Seguindo</Text>
+              </View>
             </View>
-          </View>
-          <TouchableOpacity onPress={() => navigation.navigate("Repositorios")}>
-            <Text style={estilos.repositorios}>Ver os repositórios</Text>
-          </TouchableOpacity>
-        </>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("Repositorios", { id: usuario.id })
+              }
+            >
+              <Text style={estilos.repositorios}>Ver os repositórios</Text>
+            </TouchableOpacity>
+          </>
+        )}
 
         <TextInput
           placeholder="Busque por um usuário"
           autoCapitalize="none"
           style={estilos.entrada}
+          value={nomeUsuario}
+          onChangeText={setNomeUsuario}
         />
 
-        <TouchableOpacity style={estilos.botao} onPress={Busca}>
+        <TouchableOpacity style={estilos.botao} onPress={busca}>
           <Text style={estilos.textoBotao}>Buscar</Text>
         </TouchableOpacity>
       </View>
